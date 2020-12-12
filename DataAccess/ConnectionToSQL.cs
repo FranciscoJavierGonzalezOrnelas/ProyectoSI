4eda16738c1013bd40603fe09c50c6e145887d91
Francisco González <paco_go09@hotmail.com>
2020-12-11 19:31:46 -0600

    Initial commit

---
+++ DataAccess/ConnectionToSQL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    using System.Data;
+    using System.Data.SqlClient;
+
+    public abstract class ConnectionToSQL
+    {
+        private readonly String connectionString;
+
+        public ConnectionToSQL()
+        {
+            connectionString = "Server=DESKTOP-C33O34G\\SQLEXPRESS; DataBase=CF_BOX_Final; integrated security=true";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
